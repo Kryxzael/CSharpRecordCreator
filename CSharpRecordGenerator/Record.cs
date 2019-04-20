87bed342d11c4ad9c600c0cc05475f36169b5ca1
Kryxzael <kasper.ramskjell98@gmail.com>
2019-04-20 12:50:12 +0200

    Set up the very basics

---
+++ CSharpRecordGenerator/Record.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpRecordGenerator
+{
+    public class Record
+    {
+        public string Name { get; set; }
+        public List<dynamic> Fields { get; } = new List<dynamic>();
+    }
+}
---
+++ CSharpRecordGenerator/RecordField.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpRecordGenerator
+{
+    public class RecordField
+    {
+        public string Name { get; set; }
+        public string DataType { get; set; }
+    }
+}
